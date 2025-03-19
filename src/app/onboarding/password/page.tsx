68f08a8322704d5d3a5b926eee8b1a2397c48003
Bobyson Laishram <acntanouba@gmail.com>
2025-03-19 22:41:49 +0530

    multi-step form with zod and react-hook-form

---
+++ src/app/onboarding/password/page.tsx
@@ -0,0 +1,5 @@
+import OnboardingPasswordForm from "@/features/onboarding/components/onboarding-password-form";
+
+export default function onboardingPasswordPage() {
+  return <OnboardingPasswordForm />;
+}
